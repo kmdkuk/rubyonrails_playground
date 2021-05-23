8a2b87100f6ee3ed1b67c7cc43cf38433be015ec
kouki <kouworld0123@gmail.com>
2021-05-23 21:54:27 +0900

    prepare firebase-auth-rails

---
+++ backend/config/initializers/firebase_auth_initializer.rb
@@ -0,0 +1,4 @@
+FirebaseIdToken.configure do |config|
+  config.redis = Redis.new(host: ENV.fetch('REDIS_HOST') {'redis'}, port: 6379)
+  config.project_ids = [ENV.fetch("FIREBASE_PROJECT_ID") {'firebase_project_id'}]
+end
