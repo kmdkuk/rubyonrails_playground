c5c039c5c3ffd19551c3ef5cb3c9cb161c13980e
kouki <kouworld0123@gmail.com>
2021-05-26 20:41:46 +0900

    hello world

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <br />
+          hello world!
         </p>
         <a
           className="App-link"
